0b53bd3023052c03560a3ce1f024554d8dcc59eb
SkorP Files <110429183+skorpfiles@users.noreply.github.com>
2023-05-25 17:04:16 +0200

    moved global constants to the environment variables

--- SkorpFiles.Memorizer.ReactShell/src/GlobalConstants.js
+++ SkorpFiles.Memorizer.ReactShell/src/GlobalConstants.js
@@ -1,2 +1,2 @@
-export const ApiHostUrl = 'https://localhost:7205';
-export const CookiesExpireDays = 7;
+export const ApiHostUrl = process.env.REACT_APP_API_HOST_URL;
+export const CookiesExpireDays = process.env.REACT_APP_COOKIES_EXPIRE_DAYS;
