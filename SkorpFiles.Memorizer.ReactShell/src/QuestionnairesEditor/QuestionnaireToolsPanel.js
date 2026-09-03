8977df40c51a0db8223d1ab4e385310f1590fdb4
SkorP Files <110429183+skorpfiles@users.noreply.github.com>
2026-09-03 09:21:36 +0200

    added filtering by labels

--- SkorpFiles.Memorizer.ReactShell/src/QuestionnairesEditor/QuestionnaireToolsPanel.js
+++ SkorpFiles.Memorizer.ReactShell/src/QuestionnairesEditor/QuestionnaireToolsPanel.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import QuestionnaireToolsPanelButton from './QuestionnaireToolsPanelButton';
+import QuestionnaireLabel from './QuestionnaireLabel';
 
 class QuestionnaireToolsPanel extends React.Component {
     constructor(props) {
@@ -8,12 +9,19 @@ class QuestionnaireToolsPanel extends React.Component {
 
     render() {
         return (
-            <div style={{ width: "100%", margin: "10px 0px", padding: "5px 10px", border: "1px solid white", color: "white", display: "flex", flexWrap: "wrap" }}>
-                {this.props.canBeEdited && (<QuestionnaireToolsPanelButton
-                    doAction={this.props.startAddingQuestion}
-                    text="Add a question"
-                />)}
-                {!this.props.canBeEdited && (<div>You cannot edit this questionnaire.</div>)}
+            <div>
+                <div style={{ width: "100%", margin: "10px 0px", padding: "5px 10px", border: "1px solid white", color: "white", display: "flex", flexWrap: "wrap" }}>
+                    {this.props.canBeEdited && (<QuestionnaireToolsPanelButton
+                        doAction={this.props.startAddingQuestion}
+                        text="Add a question"
+                    />)}
+                    {!this.props.canBeEdited && (<div>You cannot edit this questionnaire.</div>)}
+                </div>
+                {this.props.labelsForQuestionnaire.length > 0 && (
+                    <div style={{ display: "flex", flexDirection: "horizontal", gap: "8px", flexWrap: "wrap" }}>
+                        {this.props.labelsForQuestionnaire.map(label => (<QuestionnaireLabel key={label} labelName={label} selected={this.props.selectedLabels.includes(label)} handleChoosing={() => this.props.handleLabelChoosing(label)} />))}
+                    </div>
+                )}
             </div>
         )
     }
